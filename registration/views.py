fecdabd5bd08d1add8ed9fad08b3bffdc04358d5
Ubuntu <ubuntu@machbtech.com>
2015-01-25 07:41:35 +0000

    first commit

---
+++ registration/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, render_to_response
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+	return render_to_response("registration/index.html")
+
+def confirm(request):
+	return render_to_response("registration/confirm.html")
+
